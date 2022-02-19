55d107d7f789468a05fccb7132fb5f9feae49002
nathanjonas <natemjonas@gmail.com>
2022-02-19 15:15:26 -0500

    implement simple webpage

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <span className="header-text">CEJ EDGE LLC</span>
+        <h2 className="sub-header-text">Advanced Battery Manufacturing</h2>
+      </div>
+      <h3 className="sub-sub-header-text">
+        CEJ Edge LLC has a focus on advanced battery manufacturing, business
+        plan deployment, and general consulting while promoting a Zero(0) defect
+        focus.
+      </h3>
+    </div>
+  );
+};
