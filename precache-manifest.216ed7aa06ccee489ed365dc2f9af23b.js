05104259d193ed18d769b8b7ebac71c19ea20825
nathanjonas <natemjonas@gmail.com>
2022-03-30 21:07:57 -0400

    Updates

---
+++ precache-manifest.216ed7aa06ccee489ed365dc2f9af23b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c2f5328a90976711be418d99614ce1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92e4b92656748e639adf",
+    "url": "/static/css/main.0e721926.chunk.css"
+  },
+  {
+    "revision": "0d0e0ea1923076258949",
+    "url": "/static/js/2.4b0073a2.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/static/js/2.4b0073a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "925a35e3b4a05a126997",
+    "url": "/static/js/3.ae3fd23c.chunk.js"
+  },
+  {
+    "revision": "92e4b92656748e639adf",
+    "url": "/static/js/main.674354c0.chunk.js"
+  },
+  {
+    "revision": "a24a0c68d5c3c38f2b34",
+    "url": "/static/js/runtime-main.f729ffc8.js"
+  }
+]);
